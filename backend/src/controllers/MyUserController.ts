ba4532a313d986caa77f92ce5129fb9916155b64
Keshav Gupta <guptakeshav0710@gmail.com>
2024-07-25 18:08:47 +0530

    create user

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
-const createCurrentUser=async (req: Express.Request, res: Express.Response) => {
-    
-}
+const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { auth0id } = req.body;
+    const existingUser = await User.findOne({ auth0id });
+
+    if (existingUser) {
+      return res.status(200).send();
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error Creating User" });
+  }
+};
 
 export default {
-    createCurrentUser,
+  createCurrentUser,
 };
