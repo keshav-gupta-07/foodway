b967499b79632b7d998fc51dbe49a7384470ac01
Keshav Gupta <guptakeshav0710@gmail.com>
2024-07-25 21:37:05 +0530

    user

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateMyUser } from "@/api/MyUserApi";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const Auth0ProviderWithNavigate = ({children}: Props) => {
-  const domain=import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId=import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri=import.meta.env.VITE_AUTH0_CALLBACK_URL;
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
-    if(!domain || !clientId || !redirectUri){
-        throw new Error("Unable to intialize Auth0");
+  if (!domain || !clientId || !redirectUri) {
+    throw new Error("Unable to intialize Auth0");
+  }
+
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
     }
-    const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("USER", user);
-    };
+  };
 
-    return (
-        <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{redirect_uri: redirectUri,}} onRedirectCallback={onRedirectCallback}>
-            {children}
-        </Auth0Provider>
-    );
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: redirectUri }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export default Auth0ProviderWithNavigate;
